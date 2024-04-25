e21fa2ef6e48544812177b66859df1b6b572368e
ok-at-computers <ksfan@ucsb.edu>
2024-04-25 13:21:51 -0700

    added entity, repository, controller, and db migration file for Articles

---
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -0,0 +1,25 @@
+package edu.ucsb.cs156.example.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "articles")
+public class Articles {
+  @Id
+  private String title;
+  private String url;
+  private String explanation;
+  private String email;
+  private LocalDateTime dateAdded;
+}

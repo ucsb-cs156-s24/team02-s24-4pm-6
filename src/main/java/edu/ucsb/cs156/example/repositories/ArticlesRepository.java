9a43956c3e2b249010154cf8324507ee27ba0641
ok-at-computers <ksfan@ucsb.edu>
2024-05-02 02:41:51 -0700

    changed get to use id rather than title

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArticlesRepository extends CrudRepository<Articles, String> {
+public interface ArticlesRepository extends CrudRepository<Articles, Long> {
  
 }

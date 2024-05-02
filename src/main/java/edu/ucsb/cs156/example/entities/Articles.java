45eed2165cff4ecaf0efeca8782600b8c773f54f
ok-at-computers <ksfan@ucsb.edu>
2024-05-02 10:11:29 -0700

    made yet more additional changes to articles.java

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -3,7 +3,9 @@ package edu.ucsb.cs156.example.entities;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,7 @@ public class Articles {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+  
   private String title;
   private String url;
   private String explanation;
